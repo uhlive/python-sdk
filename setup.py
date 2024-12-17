d732ee164df0b0f57f0065b79fce48a0937425e9
Romuald Texier-Marcadé <r.texiermarcade@allo-media.fr>
2024-12-17 12:01:22 +0100

    Next version is 1.6.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="uhlive",
-    version="1.6.0",
+    version="1.6.1",
     url="https://github.com/uhlive/python-sdk",
     author="Allo-Media",
     author_email="support@allo-media.fr",
